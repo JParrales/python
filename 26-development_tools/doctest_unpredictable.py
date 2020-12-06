844b19a437c81d972d8b413e690c677bb5baa6a7
JParrales <josedparrales@gmail.com>
2020-12-06 18:56:12 -0500

    Doctest

--- 26-development_tools/doctest_unpredictable.py
+++ 26-development_tools/doctest_unpredictable.py
@@ -3,10 +3,11 @@
 class MyClass:
     pass
 
+
 def unpredictable(obj):
     """Returns a new list containing obj.
 
-    >>> unpredictable(MyClass()) #doctest: +ELLIPSIS
-    [<doctest_unpredictable.MyClass object at 0x...>]
+    >>> unpredictable(MyClass())
+    [<doctest_unpredictable.MyClass object at 0x10055a2d0>]
     """
     return [obj]
